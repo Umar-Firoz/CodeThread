3c6eb8e56636345392812b5b0efa95a674a8f414
Umar Firoz <f.umar@op.iitg.ac.in>
2026-07-09 21:16:23 +0530

    Added backed folder

---
+++ backend/src/test/java/com/example/CodeThread/CodeThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeThread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

78ced0b15aa81a23781e338c998c0feaaeceb732
Umar Firoz <f.umar@op.iitg.ac.in>
2026-07-10 14:55:25 +0530

    added security config class

--- backend/src/main/java/com/example/CodeThread/config/SecurityConfig.java
+++ backend/src/main/java/com/example/CodeThread/config/SecurityConfig.java
@@ -1,20 +1,48 @@
 package com.example.CodeThread.config;
 
+import com.example.CodeThread.security.AuthEntryPoint;
+import com.example.CodeThread.security.AuthTokenFilter;
+import com.example.CodeThread.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthTokenFilter authTokenFilter;
+    private final AuthEntryPoint authEntryPoint;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        auth->auth.requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
+                .addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex->ex.authenticationEntryPoint(authEntryPoint));
+        return http.build();
+    }
 }
 
 
